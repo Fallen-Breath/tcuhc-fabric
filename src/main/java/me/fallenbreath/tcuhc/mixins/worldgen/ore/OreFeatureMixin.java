3b1798c469050f2461e71612b44b5ebb4ea46a71
Fallen_Breath <yqx791125833@gmail.com>
2021-12-12 20:02:24 +0800

    correct ore exposing check

--- src/main/java/me/fallenbreath/tcuhc/mixins/worldgen/ore/OreFeatureMixin.java
+++ src/main/java/me/fallenbreath/tcuhc/mixins/worldgen/ore/OreFeatureMixin.java
@@ -13,8 +13,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Random;
 import java.util.function.Function;
 
-import static net.minecraft.world.gen.feature.Feature.isExposedToAir;
-
 @Mixin(OreFeature.class)
 public abstract class OreFeatureMixin
 {
@@ -23,7 +21,7 @@ public abstract class OreFeatureMixin
 	{
 		if (UhcFeatures.isValuableOreBlock(target.state.getBlock()))
 		{
-			if (!isExposedToAir(posToState, pos))
+			if (!isValidPositionForValuableOre(posToState, pos))
 			{
 				cir.setReturnValue(false);
 			}
@@ -47,5 +45,27 @@ public abstract class OreFeatureMixin
 				cir.setReturnValue(true);
 			}
 		}
+	}
+
+	/**
+	 * Not using {@link net.minecraft.world.gen.feature.Feature#isExposedToAir} here since we scan more than adjacent
+	 * blocks
+	 */
+	private static boolean isValidPositionForValuableOre(Function<BlockPos, BlockState> posToState, BlockPos pos)
+	{
+		for (int x = -1; x <= 1; x++)
+		{
+			for (int y = -1; y <= 1; y++)
+			{
+				for (int z = -1; z <= 1; z++)
+				{
+					if (posToState.apply(pos.add(x, y, z)).isAir())
+					{
+						return true;
+					}
+				}
+			}
+		}
+		return false;
 	}
 }
